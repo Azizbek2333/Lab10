8e25fbea2d29288f240ba0d7bdeb59fd5e5f4249
Azizbek2333 <250424@newuu.uz>
2025-11-21 12:57:37 +0500

    Create Problem1.cpp

---
+++ Problem1.cpp
@@ -0,0 +1,15 @@
+int main() {
+    int n1=9,n2=11, *ptr_1= nullptr, *ptr_2= nullptr, k=0;
+    double t;
+
+    //ptr_1 = &n1;
+    ptr_1 = &n2;
+    // *ptr_2 = &n2;
+    ptr_2 = &n1;
+    t = val;
+    k = val;
+    //foo() = number;
+    fun() = number;
+
+    cout << *ptr_1 << " " << *ptr_2 << " " << t << " " << a << endl;
+}
